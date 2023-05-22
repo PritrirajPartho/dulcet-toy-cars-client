66cc34e17ea3eca4dac6562f923f2781fe5a0ea7
PritrirajPartho <dapartho5616@gmail.com>
2023-05-23 03:23:20 +0600

    aos-adding

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
-import { Tooltip } from 'react-tooltip'
+import { Tooltip } from 'react-tooltip';
+import logo from '../../../public/com/logo.png'
 
 const Header = () => {
     const { logOut, user } = useContext(AuthContext);
@@ -38,6 +39,7 @@ const Header = () => {
                 }
             </ul>
           </div>
+             <img className='w-52 h-28 ' src={logo} alt="" />
              <h1 className=" normal-case text-xl font-bold ms-5 ">Dulcet-Toy-Cars</h1> 
           </div>
         <div className="navbar-center hidden lg:flex">
