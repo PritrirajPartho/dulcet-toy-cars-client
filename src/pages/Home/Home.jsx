216353a4247adfb2687a95763f95ef692616a82c
PritrirajPartho <dapartho5616@gmail.com>
2023-05-23 02:09:02 +0600

    design-completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ShopByCategory from './ShopByCategory/ShopByCategory';
 import Banner from './Banner/Banner';
 import Gallery from './Gallery/Gallery';
+import TrustBy from './TrustBy/TrustBy';
+import HappyClient from './HappyClient/HappyClient';
 
 
 const Home = () => {
@@ -10,6 +12,8 @@ const Home = () => {
              <Banner></Banner>
              <Gallery></Gallery>
              <ShopByCategory></ShopByCategory>
+             <TrustBy></TrustBy>
+             <HappyClient></HappyClient>
         </div>
     );
 };
---
+++ src/pages/Home/TrustBy/TrustBy.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import com1 from '../../../../public/com/addidas.png';
+import com2 from '../../../../public/com/ibm.png';
+import com3 from '../../../../public/com/ikea.webp';
+import com4 from '../../../../public/com/nord.png';
+import com5 from '../../../../public/com/red-bull.png';
+import com6 from '../../../../public/com/fendi.png';
+
+const TrustBy = () => {
+    return (
+    <section className='mt-8 mb-12'>
+        <h1 className='text-center text-emerald-600 text-5xl mb-4'>Trusted By</h1>
+        <div className='flex flex-row'>
+            <img className='w-60 h-28' src={com1} />
+            <img className='w-60 h-28' src={com2} />
+            <img className='w-60 h-28' src={com3} />
+            <img className='w-60 h-28' src={com4} />
+            <img className='w-60 h-28' src={com5} />
+            <img className='w-60 h-28' src={com6} />
+        </div>
+    </section>
+    );
+};
+
+export default TrustBy;
